b756a9972a7f58587fb248a98b9f18c140cbba18
Jayanta66 <jayhalder18@gmail.com>
2024-08-27 09:28:24 +0530

    React Learning

---
+++ src/scripts/App.js
@@ -0,0 +1,12 @@
+const userMessage ="Hello World";
+
+console.log(userMessage);
+console.log(userMessage);
+
+console.log(10===10);
+console.log(10===11);
+console.log(10<=1);
+
+if(10===10){
+console.log("works");
+}
