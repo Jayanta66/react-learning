ac56b63846ef5a359fd3d15d51ee49edb5ef4d99
Jayanta66 <jayhalder18@gmail.com>
2024-08-31 16:42:22 +0530

    React vite learning

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import reactImg from '../assets/react-core-concepts.png';
-import '../components/Header.css';
+import reactImg from '../../assets/react-core-concepts.png';
+import '../Header/Header.css';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
